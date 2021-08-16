9092ad27e412bdd18504fd98c4e5715fd832d336
Emil Abdulxalıqov <emilabdulxaliqov@Emils-MacBook-Air.local>
2021-08-16 10:10:54 +0400

    new access

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD0dIpMpPQ0vr7X7kdSyiptfy7aPn68FMw",
-    authDomain: "demoproject-1ad42.firebaseapp.com",
-    databaseURL: "https://demoproject-1ad42-default-rtdb.firebaseio.com",
-    projectId: "demoproject-1ad42",
-    storageBucket: "demoproject-1ad42.appspot.com",
-    messagingSenderId: "681576960302",
-    appId: "1:681576960302:web:43188800cf791b517f2abf"
-  };
+  apiKey: "AIzaSyDpVzxA1HX4oOpEjhwm5rPYT35OxnCNgaY",
+  authDomain: "rockpaperscissors-f3d80.firebaseapp.com",
+  projectId: "rockpaperscissors-f3d80",
+  storageBucket: "rockpaperscissors-f3d80.appspot.com",
+  messagingSenderId: "826709556860",
+  appId: "1:826709556860:web:e12dfd9a3f88a8d8a853f9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -1,12 +1,155 @@
-let db=firebase.database();
+'use strict';
 
-db.ref("user").set({
-    username:'Emil'
-});
+let database = firebase.database();
 
-db.ref().on('value',function(){
-    console.log('Hello world');
+let startBtn = document.querySelector("#start");
+
+let user = null;
+
+let count = 0;
+
+// let playerChoice=document.querySelectorAll()
+
+
+let bool = true;
+
+let username = document.querySelector("#username");
+
+let waiting1 = document.querySelector(".waiting1");
+
+let waiting2 = document.querySelector(".waiting2");
+
+let player1 = document.querySelector(".player1");
+
+let player2 = document.querySelector(".player2");
+
+let local = null;
+
+startBtn.addEventListener("click", function (e) {
+
+    if (waiting1.innerHTML) {
+        
+        let user = database.ref("room1").push({
+            name: username.value
+        })
+    
+        localStorage.setItem("userKey", user.key);
+    
+        database.ref(`room1/${localStorage.getItem("userKey")}`).on("value", function (snapshot) {
+            player1.innerHTML = snapshot.val().name;
+        })
+
+        waiting1.innerHTML = "";
+    } 
+    else {
+        
+        let user = database.ref("room1").push({
+            name: username.value
+        })
+    
+        localStorage.setItem("userKey", user.key);
+    
+        database.ref(`room1/${localStorage.getItem("userKey")}`).on("value", function (snapshot) {
+            player2.innerHTML = snapshot.val().name;
+        })
+
+        waiting2.innerHTML = "";
+    }
+
 })
+ 
+window.onload = (e) => {
+    if (!player1.innerHTML) {
+        waiting1.innerHTML = "";
+        database.ref(`room1/${localStorage.getItem("userKey")}`).on("value", function (snapshot) {
+            player1.innerHTML = snapshot.val().name;
+        })
+    } else {
+        player1.innerHTML = "";
+        waiting1.innerHTML = "Waiting for Player1"
+    }
+
+    if (!player2.innerHTML) {
+        waiting2.innerHTML = "";
+        database.ref(`room1/${localStorage.getItem("userKey")}`).on("value", function (snapshot) {
+            player2.innerHTML = snapshot.val().name;
+        })
+    }
+
+}
+
+if (!player1.innerHTML) {
+    let rockBtn = document.querySelector("#rock1");
+
+    let scissorsBtn = document.querySelector("#scissors1");
+
+    let paperBtn = document.querySelector("#paper1");
+
+    rockBtn.addEventListener("click", function (e) {
+        database.ref(`room1/${localStorage.getItem("userKey")}/choices`).set({
+            choice: 'rock'
+        })
+        document.querySelector(".left-player-choice").innerHTML = "Rock";
+
+        console.log('Rock1');
+    })
+    scissorsBtn.addEventListener("click", function (e) {
+        database.ref(`room1/${localStorage.getItem("userKey")}/choices`).set({
+            choice: 'scissors'
+        })
+        document.querySelector(".left-player-choice").innerHTML = "Scissors";
+        console.log('Scissors1');
+    })
+    paperBtn.addEventListener("click", function (e) {
+        database.ref(`room1/${localStorage.getItem("userKey")}/choices`).set({
+            choice: 'paper'
+        })
+        document.querySelector(".left-player-choice").innerHTML = "Paper";
+        console.log('Paper1');
+    })
+
+}
+
+if (!player2.innerHTML) {
+    let rockBtn = document.querySelector("#rock2");
+
+    let scissorsBtn = document.querySelector("#scissors2");
+
+    let paperBtn = document.querySelector("#paper2");
+
+    rockBtn.addEventListener("click", function (e) {
+        database.ref(`room1/${localStorage.getItem("userKey")}/choices`).set({
+            choice: 'rock'
+        })
+        document.querySelector(".right-player-choice").innerHTML = "Rock";
+
+        console.log('Rock2');
+    })
+    scissorsBtn.addEventListener("click", function (e) {
+        database.ref(`room1/${localStorage.getItem("userKey")}/choices`).set({
+            choice: 'scissors'
+        })
+        document.querySelector(".left-player-choice").innerHTML = "Scissors";
+        console.log('Scissors2');
+    })
+    paperBtn.addEventListener("click", function (e) {
+        database.ref(`room1/${localStorage.getItem("userKey")}/choices`).set({
+            choice: 'paper'
+        })
+        document.querySelector(".left-player-choice").innerHTML = "Paper";
+        console.log('Paper2');
+    })
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
